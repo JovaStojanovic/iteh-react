73b55b87215b749a19ced32872f88eb0668290a6
jovan <jovan.stojanovic36@gmail.com>
2023-02-10 14:04:42 +0100

    Dodat react-router-dom, pocetna strana i ruta ka njoj

---
+++ src/components/Main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Main() {
+
+    return (
+        <>
+        <div className='header'>
+        <img className='logoImg' src="https://img.freepik.com/free-vector/round-sticker-guitar-school-vector-illustration-black-promotional-label-advertising-rock-music-lessons_74855-10863.jpg?w=740&t=st=1675965157~exp=1675965757~hmac=5e2c5e2814b3e211533ee3f9a37376a16d3e1d5cde66be767ac35ad3fde7c43d"></img>
+        </div>
+        <div className='body'>
+            <div className='aboutus'>
+                <img className='portfolioImg' src="https://images.pexels.com/photos/64111/pexels-photo-64111.jpeg?auto=compress&cs=tinysrgb&w=600"></img>
+            </div>
+            <div className='us'>
+                <h1>GUITAR SHOP</h1>
+                <p><b>GUITAR SHOP</b> je prodavnica koja je nastala 2004. godine sa namerom da obezbedjuje celo trziste Srbije, a narocito Beograda.
+                    U ponudi imamo gitare svih najpoznatijih proizvodjaca iz celog sveta, kako akusticnih, tako i elektricnih gitara.
+                </p>
+            </div>
+        </div>
+        <div className='body'>
+            <div className='us'>
+                <p><b>NASA MISIJA</b> je da budemo najveca prodavnica gitara u Beogradu, ali i u Srbiji, a takodje da nasi kupci budu zadovoljni nasim proizvodima.
+                    S tim u vezi nudimo isprobavanje gitare u nasoj prodavnici, kao i povrat novca ako niste zadovoljni gitarom. Pored toga, moguce je da donesete vase gitare na servis,
+                    gde ce im biti zamenjene zice, kao i premazani pragovi uljem.
+                </p>
+            </div>
+            <div className='aboutustwo'>
+                <img className='portfolioImg' src="https://images.pexels.com/photos/63695/pexels-photo-63695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Main
